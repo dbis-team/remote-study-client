0766dea740fde62ae8df1202fce5614c81cfa252
Mykyta Zviahin <mykytazviahin@yahoo.com>
2020-11-30 15:45:20 +0200

    fix problem

--- src/pages/EducationSets/index.tsx
+++ src/pages/EducationSets/index.tsx
@@ -3,38 +3,20 @@ import { connect } from 'react-redux';
 import { Box } from '@material-ui/core';
 
 import { actions as userActions } from 'store/reducers/user';
-import { UserApiDomainService } from 'services/api/domains/UserApiService';
 import { EducationSet } from 'components/EducationSet';
 import { AddEducationSet } from 'components/AddEducationSet';
-import { Feed } from 'components/Feed';
 
 export interface IEducationSetsPageProps {
   addUserData: typeof userActions.addUserData;
 }
 
-const EducationSetsPage: React.FC<IEducationSetsPageProps> = ({ addUserData }) => {
-  React.useEffect(() => {
-    /**
-     * It's just tests of API and redux
-     */
-    const userApiService = new UserApiDomainService();
-
-    userApiService.getUsers()
-      .then((users) => (users.isLeft()
-        ? console.error(users.getLeft())
-        : console.info(users.getRight())));
-
-    addUserData({ Id: '123' });
-  }, [addUserData]);
-
-  return (
-    <Box>
-      <EducationSet />
-      <EducationSet />
-      <AddEducationSet />
-    </Box>
-  );
-};
+const EducationSetsPage: React.FC<IEducationSetsPageProps> = ({ addUserData }) => (
+  <Box>
+    <EducationSet />
+    <EducationSet />
+    <AddEducationSet />
+  </Box>
+);
 
 const mapDispatchToProps = {
   addUserData: userActions.addUserData,
--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -3,34 +3,17 @@ import { connect } from 'react-redux';
 import { Box } from '@material-ui/core';
 
 import { actions as userActions } from 'store/reducers/user';
-import { UserApiDomainService } from 'services/api/domains/UserApiService';
 import { Schedule } from 'components/Schedule';
 
 export interface ISchedulePageProps {
   addUserData: typeof userActions.addUserData;
 }
 
-const ScheduleSetsPage: React.FC<ISchedulePageProps> = ({ addUserData }) => {
-  React.useEffect(() => {
-    /**
-     * It's just tests of API and redux
-     */
-    const userApiService = new UserApiDomainService();
-
-    userApiService.getUsers()
-      .then((users) => (users.isLeft()
-        ? console.error(users.getLeft())
-        : console.info(users.getRight())));
-
-    addUserData({ Id: '123' });
-  }, [addUserData]);
-
-  return (
-    <Box>
-      <Schedule />
-    </Box>
-  );
-};
+const ScheduleSetsPage: React.FC<ISchedulePageProps> = ({ addUserData }) => (
+  <Box>
+    <Schedule />
+  </Box>
+);
 
 const mapDispatchToProps = {
   addUserData: userActions.addUserData,
